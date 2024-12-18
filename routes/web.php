b7a80cfea48efb1a63feef03d81fafe195d23d6f
alisk <alicka.martyna@gmail.com>
2024-12-18 23:07:23 +0100

    add admin page and form

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
-use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Front\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
+Route::get('admin', [AdminController::class, 'admin_panel']);
